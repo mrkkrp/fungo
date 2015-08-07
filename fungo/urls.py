d384664e430713cf7de117cf693a8f574be30928
mrkkrp <markkarpov@opmbx.org>
2015-08-08 01:32:01 +0600

    added categories and dynamic pages

--- fungo/urls.py
+++ fungo/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from fungo import views
 
 urlpatterns = [
-    url(r'^$',      views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
+    url(r'^$',         views.index, name='index'),
+    url(r'^category/(?P<category_name_url>[\w\-]+)/$',
+        views.category, name='category'),
+    url(r'^about/',    views.about, name='about'),
 ]
