86c8259db61806127a4811d68d8a98a254348882
mrkkrp <markkarpov@opmbx.org>
2015-08-07 20:19:10 +0600

    messing with models

---
+++ fungo/migrations/0002_auto_20150807_1351.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fungo', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=128, unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=128)),
+                ('url', models.URLField()),
+                ('views', models.IntegerField(default=0)),
+                ('category', models.ForeignKey(to='fungo.Category')),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='choice',
+            name='question',
+        ),
+        migrations.DeleteModel(
+            name='Choice',
+        ),
+        migrations.DeleteModel(
+            name='Question',
+        ),
+    ]
--- fungo/models.py
+++ fungo/models.py
@@ -4,20 +4,38 @@ import datetime
 
 # Models
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
-        return self.question_text
+        return self.name
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    class Meta:
+        verbose_name_plural = "Categories"
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.title
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+
+#     def __str__(self):
+#         return self.question_text
+
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+#     def __str__(self):
+#         return self.choice_text
